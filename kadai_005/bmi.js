8624667be89eb62ff2e72d767f84e9618e83132f
Asami-hello <imada.asami@gmail.com>
2025-05-15 22:49:19 +0900

    kadai_005の再提出

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,5 +1,10 @@
-// 変数の宣言と値の代入
-let bmi = (68/(1.7*1.7));
+// 体重の変数の宣言と値の代入
+let weight = 68;
 
-// コンソールの出力
-console.log(bmi)
+// 身長の変数の宣言と値の代入
+let height = 1.7;
+
+// BIMを計算
+console.log(weight / (height * height))
+
+
