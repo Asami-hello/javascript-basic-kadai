35230efdc42c3f36f6ffe7c204274b991b3ebb21
Asami-hello <imada.asami@gmail.com>
2025-05-18 23:07:24 +0900

    kadai_014の再提出

--- kadai_014/square.js
+++ kadai_014/square.js
@@ -1,8 +1,8 @@
-// 関数を定義する
+// 関数を定義し、戻り値を返す
 const calculateTotal = (num) => {
-    console.log(num * num);
+    return num * num;
 }
 
-// 引数を指定する
-calculateTotal(10);
+// 戻り値の二乗を出力する
+console.log(calculateTotal(10));
 
